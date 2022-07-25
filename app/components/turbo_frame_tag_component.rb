b8cfdd097eebbf3b68e52eb37ee8dcda86c1782d
Matheus Poli Camilo <matheus.camilo@nuuvem.com>
2022-07-25 14:50:21 -0300

    Initial commit

---
+++ app/components/turbo_frame_tag_component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class TurboFrameTagComponent < ViewComponent::Base
+
+end
---
+++ app/controllers/test_view_component_controller.rb
@@ -0,0 +1,9 @@
+class TestViewComponentController < ApplicationController
+  def index
+    
+  end
+
+  def update
+    
+  end
+end
